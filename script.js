7f5b7b9355fc214e930a00e20882bfa880819c9b
Aman <amangupta3579@gmail.com>
2023-05-26 21:37:36 +0530

    Js

---
+++ script.js
@@ -0,0 +1,33 @@
+// // console.log(console)
+// // console.log("hello")
+// // console.warn("hello")
+// // console.error("hello")
+
+// console.log(window.innerHeight)
+// console.log(window.innerWidth)
+// window.alert('Hello world')
+
+// const name = window.prompt("What is your name?")
+// console.log(name)
+
+const name = ['John', 'Aman', 'Ajay'];
+console.log(name)
+
+const obj1 = {
+    name1: 'Aman',
+    age: 30,
+    isMarried : false,
+    greet:function(){
+        console.log('Hello!!!!!!')
+    },
+    address:{
+        city:'Delhi',
+        pin: 110067,
+        state: 'Delhi'},
+        hobbies:['football', 'Cricket'],
+        obj1: obj1
+}
+obj1.greet()
+
+console.log(obj1.address)
+console.log(obj1)
