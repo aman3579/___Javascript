7fe5f4b979664841767c64c1c85ff252e0c4083e
Aman <amangupta3579@gmail.com>
2023-06-02 01:17:23 +0530

    Day-25 DOM

---
+++ DOM.js
@@ -0,0 +1,18 @@
+// console.dir(node)
+// node.innerHTML = 'Hello world'/\
+
+// console.log(document.body)
+
+//methods for retrieving the DOM nodes
+// const node = document.getElementById('heading')
+// // node.innerHTML = `<i>hello world</i>  `  //replaces the data in the elenment with the id heading
+// console.log(node)   //gives a node
+// // console.dir(node)   //gives all the properties
+// // console.log(node.innerHTML)    //GIVES the content of the id heading in this case is to do list
+
+// const item = document.getElementsByClassName('list-item')
+// console.log(item)
+// const tag = document.getElementsByTagName('li')
+
+const element = document.querySelector('li')   //querySelector has two types 2nd variant is querySelectorAll
+console.log(element)
