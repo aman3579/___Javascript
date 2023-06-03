07cb75cad72b3f682f50a0ec8677c7af8cae340e
Aman <amangupta3579@gmail.com>
2023-06-03 16:53:11 +0530

    Day-26-Dom-Methods

---
+++ DOM_Methods.js
@@ -0,0 +1,38 @@
+const heading = document.querySelector('h1')
+// console.log(heading.innerHTML)   //it gives the inner part that is text of the heading 
+// console.log(heading.textContent)   //prints out the text part of the heading in the console
+
+
+// const body = document.querySelector('body')
+// console.log(body)
+
+// console.log(body.innerHTML)
+// console.log(heading.innerHTM
+// heading.innerHTML = "Using heading.innerHTML we can write stuff inside the heading of the document"
+
+// heading.innerHTML =  '<span style="color:red"> Hello </span> world!!! '
+
+const content = document.querySelector('p')
+content.style = ' background:olive; padding:100px; color:blueviolet '
+
+//we can change the styles of the body using following
+// body.style = ' margin:0px; padding:0px; box-sizing:border-box'
+
+
+
+// const log = ()=>{
+//     console.log('i am the log')
+// }
+// setInterval(log, 10)   //it will print the output of the log function severl times
+
+
+// const date = new Date()
+// console.log(date)
+
+
+let count =0;
+const log = () =>{
+    heading.innerHTML = count++
+}
+setInterval(log, 1000)
+
