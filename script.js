702fb8328f76040d28aec5a91563b33e409f26f4
Aman <amangupta3579@gmail.com>
2024-01-04 17:59:12 +0530

    functions

--- script.js
+++ script.js
@@ -12,34 +12,71 @@
 // console.log(names)
 
 //json- javascript object notation. This is like a python dictionary in javascript
-const car={
-    name:"audi",
-    model:"5676"
-}
-const obj = {
-    name:'aman',
-    age:20,
-    address:"delhi",
-    greet: function greet(){
-        console.log("Hello !!!")
-    },
-address:{
-    town:"delhi",
-    pin:8788,
-    state:"Delhi"
-},
-newcar:car   //nesting one json to other
-}
-console.log(obj)
-console.log(obj.newcar)
-// obj.greet()
-// console.log(obj.age)
-console.log(obj["address"])
-
+// const car={
+//     name:"audi",
+//     model:"5676"
+// }
+// const obj = {
+//     name:'aman',
+//     age:20,
+//     address:"delhi",
+//     greet: function greet(){
+//         console.log("Hello !!!")
+//     },
+// address:{
+//     town:"delhi",
+//     pin:8788,
+//     state:"Delhi"
+// },
+// newcar:car   //nesting one json to other
+// }
+// console.log(obj)
+// console.log(obj.newcar)
+// // obj.greet()
+// // console.log(obj.age)
+// console.log(obj["address"])
 
 //functions
-function greet(){
-    console.log("hello")
+
+// function greet(Name, age){
+//     console.log("hello!! " + Name 
+//     + " your age is "+age)
+// }
+// const result = greet('john',88)   //ptrint hello
+// console.log(result)    //print undefined because function greet doesnot return something, so it will give undefined.
+
+// function func(name,age){
+//     console.log(name,age)
+// }
+// func(12,22)
+
+
+//function overloading
+// greet1('rohan',88) 
+// function greet1(Name, age){
+//     console.log("hello!! " + Name 
+//     + " your age is "+age)
+// }
+// greet2('aman', 34,'delhi')
+// function greet2(Name, age, city){
+//     console.log("hello!! " + Name 
+//     + " your age is "+age+ " and your city is "+ city)
+// }
+
+greet1('greet2',88) 
+function greet1(Name, age){
+    console.log("hello!! " + Name 
+    + " your age is "+age)
 }
-greet()
+greet2('greet1', 34)  //here it will not run because we have called a variable before its declaration
+const greet2 = function(Name, age){
+    console.log("hello!! " + Name 
+    + " your age is "+age)
+}
+
+
+  
+//no overloading in the above case when twio functions are called.
+//js takes that function with bigger parameters in the functions
+
 
