96a90ec1565a5b8e86cc1b5186631be9e27883bf
Aman <amangupta3579@gmail.com>
2024-01-04 19:31:08 +0530

    Playing with obj

--- constructor.js
+++ constructor.js
@@ -21,18 +21,56 @@
 // console.log(car1)
 
 
-class Car{
-    age=30
-    adult=true
-    constructor(brand,name){
-        this.name = name
-        this.brand = brand
-        this.age=22   //we can modify variable inside constructor
-    }
-    greet(){
-        console.log("hello "  +this.name + " model is " + this.brand)
-    }
+// class Car{
+//     age=30
+//     adult=true
+//     constructor(brand,name){
+//         this.name = name
+//         this.brand = brand
+//         this.age=22   //we can modify variable inside the constructor
+//     }
+//     greet(){
+//         console.log("hello "  +this.name + " model is " + this.brand)
+//     }
+// }
+// const car1 = new Car('Mercedes','S600')
+// car1.greet()
+
+
+// class Car{
+//     age=30
+//     adult=true
+//     constructor(brand,name){
+//         console.log(this)
+//     }
+//     greet(){
+//         console.log("hello "  +this.name + " model is " + this.age)
+//     }
+// }
+
+// function Bike(){
+//     console.log(this)
+// }
+
+// const bike1 = new Bike() //when new has been used before a function call then it acts as a constructor
+// console.log(bike1)
+
+// function Bike(brand, name){
+//     this.brand = brand;
+//     this.name = name;
+// }
+// const bike1 = new Bike('ktm', 'duke') //when new has been used before a function call then it acts as a constructor
+// console.log(bike1)
+
+const obj = {
+    name:'john',
+    age:30
 }
-const car1 = new Car('Mercedes','S600')
-car1.greet()
+const obj2 = obj
+console.log(obj2)
+
+obj.name ="Doe" 
+obj.age=33     //name will be changed
+
+console.log(obj2)
 
